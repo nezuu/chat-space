e7b2868ac4783df34370bf6ce642b4429c8867f7
比嘉将吾 <handball.7@i.softbak.jp>
2018-09-05 15:58:44 +0900

    edit incremental

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -13,7 +13,6 @@ function appendProduct(user) {
   $("#user-search-field").on("keyup", function(e) {
     e.preventDefault();
     var input = $("#user-search-field").val();
-    console.log(input);
 
     $.ajax({
       type: 'GET',
@@ -24,7 +23,6 @@ function appendProduct(user) {
 
 
     .done(function(users) {
-      console.log('ajax成功')
       $("#user-search-result").empty();
       if (users.length !== 0 && input.length !== 0)  {
         users.forEach(function(user){
@@ -36,5 +34,28 @@ function appendProduct(user) {
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
+  });
+});
+
+
+$(function() {
+  function movementProduct(name, id) {
+    var user =  `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                   <input name='group[user_ids][]' type='hidden' value=${id}>
+                   <p class='chat-group-user__name'>${name}</p>
+                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                 </div>`
+    return user
+ }
+
+  $(document).on("click",".user-search-add", function(){
+    var name = $(this).data('user-name');
+    var id = $(this).data('user-id');
+    user = movementProduct( name, id )
+    $(this).parent().remove();
+    $(".chat-group-form__field__middle").append(user);  });
+
+
+  $(document).on("click",".user-search-remove", function(){    $(this).parent().remove();
   });
 });
