1a3383606943370eb4ded062facf648df793fbc8
比嘉将吾 <handball.7@i.softbak.jp>
2018-09-09 16:33:59 +0900

    Edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/nezu/.ssh/key_pem.pem']
+                  keys: ['/Users/nezu/.ssh/shogo.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
