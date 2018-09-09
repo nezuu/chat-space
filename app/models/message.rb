52e389c045891f7b4ea74546f6533ec6f5b9682c
比嘉将吾 <handball.7@i.softbak.jp>
2018-09-09 17:34:51 +0900

    Test

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
    belongs_to :group
    belongs_to :user
-
+   #rkugaki
    validates :content, presence: true, unless: :image?
    mount_uploader :image, ImageUploader
 end
