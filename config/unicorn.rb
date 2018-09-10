bbad0095257ba9e5dbc18e3a95f316036d9ae199
比嘉将吾 <handball.7@i.softbak.jp>
2018-09-10 10:22:10 +0900

    Add

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,9 @@
 app_path = File.expand_path('../../../', __FILE__)
+
+
+
 working_directory "#{app_path}/current"
+
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
